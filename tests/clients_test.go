005115d5d0e039055086233b0d1a4abea3ca1215
Dmitry <yashulyak@gmail.com>
2019-06-21 11:48:14 +0300

    Prototype churn simulator

--- tests/clients_test.go
+++ tests/clients_test.go
@@ -3,13 +3,12 @@ package tests
 import (
 	"context"
 	"crypto/rand"
-	"fmt"
+	"math/big"
 	"testing"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/log"
-	"github.com/montanaflynn/stats"
 	"github.com/status-im/status-console-client/protocol/gethservice"
 	"github.com/status-im/status-scale/client"
 	"github.com/status-im/status-scale/cluster"
@@ -45,11 +44,7 @@ func TestClientsExample(t *testing.T) {
 	// FIXME(dshulyak) if addr is not provided comcast will use both iptables and ip6tables to insert mangle rules
 	// ip6tables fails in the container on my enviornment due to lack of kernel module
 	require.NoError(t, c.EnableConditionsGloobally(context.TODO(), network.Options{TargetAddr: c.IPAM.String(), Latency: 25}))
-	rtt := &RTTMeter{
-		chat:     chat,
-		sender:   c.GetUser(0),
-		receiver: c.GetUser(1),
-	}
+	rtt := client.NewRTTMeter(chat, c.GetUser(0), c.GetUser(1))
 	// TODO(dshulyak) figure out how to measure distance between two peers.
 	// one way is to get peers from one of the user and do breadth-first search
 	log.Info("started metering latency")
@@ -59,88 +54,63 @@ func TestClientsExample(t *testing.T) {
 		"latency for 99.9 percentile", rtt.Percentile(99.9))
 }
 
-type RTTMeter struct {
-	chat gethservice.Contact
-
-	sender, receiver *cluster.Client
-	samples          []float64
+func NewChurnSim(participants []*cluster.Client, total time.Duration, churn int) *ChurnSim {
+	live := total / time.Duration(churn)
+	jitter := total / time.Duration(2)
+	lth := len(participants)
+	liveSince := make([]time.Time, lth)
+	now := time.Now()
+	for i := range liveSince {
+		liveSince[i] = now
+	}
+	return &ChurnSim{
+		live:         live,
+		total:        total,
+		jitter:       jitter,
+		participants: participants,
+		offline:      make([]bool, lth),
+		offlineSince: make([]time.Time, lth),
+		liveSince:    liveSince,
+	}
 }
 
-func (m *RTTMeter) MeterSequantially(count int) error {
-	for i := 0; i < count; i++ {
-		err := m.meter(i)
+// ChurnSim controls live and offline period for each participant.
+type ChurnSim struct {
+	live         time.Duration
+	total        time.Duration
+	jitter       time.Duration // jitter is a half of the total
+	participants []*cluster.Client
+	liveSince    []time.Time
+	offlineSince []time.Time
+	offline      []bool
+}
+
+func (c *ChurnSim) Control(ctx context.Context) error {
+	for i := range c.participants {
+		err := c.control(ctx, i)
 		if err != nil {
 			return err
 		}
 	}
-	return nil
 }
 
-func (m RTTMeter) Percentile(percent float64) float64 {
-	rst, err := stats.Percentile(m.samples, percent)
-	if err != nil {
-		return 0
-	}
-	return rst
-}
-
-func (m *RTTMeter) send(i int) (time.Time, error) {
-	tick := time.Tick(20 * time.Millisecond)
-	after := time.After(10 * time.Minute)
-	payload := fmt.Sprintf("hello receiver: %d", i)
-	// TODO(dshulyak) add util PollImmediatly(func(context.Context) error, period, timeout time.Duration)
-	for {
-		select {
-		case <-tick:
-			sent := time.Now()
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			err := client.ChatClient(m.sender.Rpc()).Send(ctx, m.chat, payload)
-			cancel()
-			if err != nil {
-				log.Debug("can't send msg", "payload", payload, "error", err)
-				continue
-			}
-			return sent, nil
-		case <-after:
-			return time.Time{}, fmt.Errorf("failed to send a message %s", payload)
+func (c *ChurnSim) control(ctx context.Context, i int) error {
+	if time.Since(c.liveSince[i]) > c.live && !c.offline[i] {
+		err := c.participants[i].Stop(ctx)
+		if err != nil {
+			return err
 		}
+		c.offlineSince[i] = time.Now()
+		c.offline[i] = true
 	}
-}
-
-func (m *RTTMeter) receive(i int) error {
-	tick := time.Tick(20 * time.Millisecond)
-	after := time.After(10 * time.Minute)
-	payload := fmt.Sprintf("hello receiver: %d", i)
-	for {
-		select {
-		case <-tick:
-			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-			msgs, err := client.ChatClient(m.sender.Rpc()).Messages(ctx, m.chat, int64(i))
-			cancel()
-			if err != nil {
-				return err
-			}
-			for _, msg := range msgs {
-				if msg.Text == payload {
-					return nil
-				}
-			}
-		case <-after:
-			return fmt.Errorf("failed waiting for a message with payload %s", payload)
+	total := c.jitter
+	total += time.Duration(rand.Int64(rand.Reader, new(big.Int).SetInt64(c.jitter)))
+	if time.Since(c.offlineSince[i]) > total {
+		err := c.participants[i].Start(ctx)
+		if err != nil {
+			return err
 		}
+		c.liveSince[i] = time.Now()
+		c.offline[i] = false
 	}
-}
-
-func (m *RTTMeter) meter(i int) error {
-	sent, err := m.send(i)
-	if err != nil {
-		return err
-	}
-	err = m.receive(i)
-	if err != nil {
-		return err
-	}
-	log.Debug("latency for msg", "i", i, "duration", time.Since(sent))
-	m.samples = append(m.samples, time.Since(sent).Seconds())
-	return nil
 }
